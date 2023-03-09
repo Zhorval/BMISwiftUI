25d2b631ead0d85fd541cce16fa495ea282ca957
Zhorval <pablo.cebrian@hotmail.com>
2023-03-09 10:50:10 +0100

    Change view

--- BMISwiftUI/ContentView.swift
+++ BMISwiftUI/ContentView.swift
@@ -8,14 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var weigth:String = ""
+    @State private var height:String = ""
+    @State private var bmi:Double = 0.0
+    @State private var clasifier:String = ""
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        VStack{
+            Text("IBM calculator").font(.title2)
+            TextField("Enter weight (in kilograms)", text: $weigth)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .border(.black)
+            
+            TextField("Enter height (in meters)", text: $height)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .border(.black)
+            
+            Button(action:{
+                let weight = Double(self.weigth)!
+                let height = Double(self.height)!
+                self.bmi = weight/(height * height)
+                
+                if self.bmi < 18.5 {
+                    clasifier = "Underweight"
+                } else if self.bmi < 24.9 {
+                    clasifier = "Healty weight"
+                } else if self.bmi < 29.9 {
+                    clasifier = "OverWeight"
+                } else {
+                    clasifier = "Obese"
+                }
+                
+            }){
+                Text("Calculate BMI")
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(.blue)
+            }
+            Text("BMI: \(bmi,specifier: "%.1f"),\(clasifier)").font(.title).padding()
+                
+
+        }.padding(50)
     }
 }
 
