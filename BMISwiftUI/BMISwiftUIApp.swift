f2311f5239a3dfabb4a4906983eb48e03537b719
Zhorval <pablo.cebrian@hotmail.com>
2023-03-09 10:39:02 +0100

    Initial Commit

---
+++ BMISwiftUI/BMISwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMISwiftUIApp.swift
+//  BMISwiftUI
+//
+//  Created by Pablo  on 9/3/23.
+//
+
+import SwiftUI
+
+@main
+struct BMISwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
